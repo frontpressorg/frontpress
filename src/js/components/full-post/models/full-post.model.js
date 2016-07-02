b91e80c45407f0687345523bb0662a6df79528ec
Jota Teles <josetelesmaciel@gmail.com>
2016-07-01 23:27:36 -0300

    . #3 - adicionando nomes de categorias e tags ao detalhe de post

--- src/js/components/full-post/models/full-post.model.js
+++ src/js/components/full-post/models/full-post.model.js
@@ -6,17 +6,20 @@
 
 	function FullPostModel(PostsApi, $q){
 		var model = {
-			title: null,
+			categoryNames: null,
 			content: null,
-			featuredImage: null,
-			setTitle: setTitle,
-			setContent: setContent,
-			setFeaturedImage: setFeaturedImage,
-			loadFullPostBySlug: loadFullPostBySlug,
-			isLoadingFullPost: false,
 			date: null,
-			setDate: setDate
-			//Data, url curta, título, tags e categorias			
+			featuredImage: null,
+			isLoadingFullPost: false,
+			loadFullPostBySlug: loadFullPostBySlug,
+			setCategoryNames: setCategoryNames,
+			setContent: setContent,
+			setDate: setDate,
+			setFeaturedImage: setFeaturedImage,
+			setTagNames: setTagNames,
+			setTitle: setTitle,
+			tagNames: null,
+			title: null,	
 		};
 
 		function setTitle(title){
@@ -35,16 +38,28 @@
 			model.featuredImage = featuredImage;
 		}
 
+		function setCategoryNames(categoryNames){
+			model.categoryNames = categoryNames;
+		}
+
+		function setTagNames(tagNames){
+			model.tagNames = tagNames;
+		}
+
 		function loadFullPostBySlug(slug){
 			var defer = $q.defer();
 
 			model.isLoadingFullPost = true;
 			var postPromise = PostsApi.getPostBySlug(slug);
 			postPromise.success(function(result){
+				var categoryNames = JSON.search(result.categories, '//name');
+				var tagNames = JSON.search(result.tags,'//name');
 				model.setTitle(result.title);
 				model.setContent(result.content);
 				model.setFeaturedImage(result.featured_image);
-				model.setDate(result.date);
+				model.setDate(result.date);				
+				model.setCategoryNames(categoryNames);
+				model.setTagNames(tagNames);
 				model.isLoadingFullPost = false;
 				defer.resolve();
 			});
