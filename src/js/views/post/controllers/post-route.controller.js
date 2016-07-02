78ca0ddf3651bb3d6aaf2ec8d834c81ff52939a0
Jota Teles <josetelesmaciel@gmail.com>
2016-07-01 23:46:05 -0300

    sem-issue - removendo codigo duplicado da rota de posts

--- src/js/views/post/controllers/post-route.controller.js
+++ src/js/views/post/controllers/post-route.controller.js
@@ -3,15 +3,8 @@
 
 	angular.module('frontpress.views.post').controller('PostRouteController', PostRouteController);
 
-    function PostRouteController(FullPostModel, $stateParams, MetadataManagerModel){
+    function PostRouteController(){
     	var vc = this;
-    	var postSlug = $stateParams.postSlug;
-    	var fullPostPromise = FullPostModel.loadFullPostBySlug(postSlug);
-        MetadataManagerModel.init();
-    	
-    	fullPostPromise.then(function(result){
-    		MetadataManagerModel.setPageTitle(FullPostModel.title);    		
-    	});
 	}
 
 })();
