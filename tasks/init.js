275b304fa6b0597db034e0d053d5a5901a57c22d
Jota Teles <jose.maciel@vivadecora.com.br>
2016-07-11 10:28:46 -0300

    [#29] - Pequenas alteracoes no prompt do gulp init

--- tasks/init.js
+++ tasks/init.js
@@ -3,29 +3,30 @@ var inquirer = require('inquirer');
 var fs = require('fs');
 
 module.exports = function() {
-	
+
     var questions = [{
         type: 'input',
         name: 'restApiUrl',
-        message: 'What is your wordpress rest api root url?'
+        message: 'Wordpress rest api url:',
+        default: 'https://public-api.wordpress.com/rest/v1.1/sites/en.blog.wordpress.com'
     },{
 		type: 'confirm',
         name: 'useDiqus',
         message: 'Do you want to use disqus on this blog?',
-        default: false
+        default: true
     },{
         when: function (response) {
             return response.useDiqus;
         },
         name: 'disqusShortname',
-        message: 'Nice! What is your disqus shortname?'        
+        message: 'Nice! Your disqus shortname:'
     }];
 
-    function processResponses(responses){    
+    function processResponses(responses){
         fs.writeFile('./frontpress.json', JSON.stringify(responses), { overwrite: true }, function (err) {
           if (err) throw err;
           console.log('Thanks for using frontpress!');
-        });        
+        });
     }
 
     var task;
@@ -34,11 +35,11 @@ module.exports = function() {
         console.log('Let\'s start by settings you blog information.');
         task = inquirer.prompt(questions).then(processResponses);
     }
-    
+
 
 	return task;
 
-	
+
 
 
 }
