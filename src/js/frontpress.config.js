428e388848b0b8e52dd7167442c6341febec56d0
Jota Teles <josetelesmaciel@gmail.com>
2016-08-08 00:22:00 -0300

    closes #31 - desabilita classes de debug do angular

--- src/js/frontpress.config.js
+++ src/js/frontpress.config.js
@@ -1,9 +1,10 @@
 angular.module("frontpress").config(frontpressConfig);
 
-function frontpressConfig($interpolateProvider, $httpProvider, $urlRouterProvider, $locationProvider, BlogApiProvider, PostsApiProvider, $FrontpressProvider){
+function frontpressConfig($interpolateProvider, $httpProvider, $urlRouterProvider, $locationProvider, BlogApiProvider, PostsApiProvider, $FrontpressProvider, $compileProvider){
     $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
     $locationProvider.html5Mode(true);
     $FrontpressProvider.configure.load();
+    $compileProvider.debugInfoEnabled(false);
 }
