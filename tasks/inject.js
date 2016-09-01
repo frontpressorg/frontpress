a75da95a48aacdfba5f8f3c0e06a0220b014e14c
Alison <alisonmonteiro.10@gmail.com>
2016-08-31 23:38:48 -0300

    [#32] - Minifcando os .js em um único arquivo com o parametro --production

--- tasks/inject.js
+++ tasks/inject.js
@@ -1,20 +1,27 @@
 var gulp = require('gulp');
 var inject = require('gulp-inject');
+var argv = process.argv;
 
 module.exports = function() {
 	var staticFilesList = [
 		'./build/css/**/*.css',
-		'./build/js/lib/external.js',
-		'./build/js/app.js',
 		'!gulpfile.js',
 		'!./tasks/*.js',
 	];
+
+    if (argv.indexOf('--production') !== -1) {
+        staticFilesList.push('./build/js/main.js')
+    } else {
+        staticFilesList.push('./build/js/dev/external.js', './build/js/dev/main.js');
+    }
 
 	var injectOptions = {
 		ignorePath: 'build/'
 	};
 
+    var injectFiles = gulp.src(staticFilesList, {read: true});
+
 	return gulp.src('src/index.html')
-		.pipe(inject(gulp.src(staticFilesList, {read: true}), injectOptions))
+		.pipe(inject(injectFiles, injectOptions))
 		.pipe(gulp.dest('./build'));
 };
