2f417509ad5d83fe064594b616ba8e0df5d445a9
Jota Teles <josetelesmaciel@gmail.com>
2016-10-05 23:28:52 -0300

    [#46]- Diretiva para post excerpt dependendo da versao de api

---
+++ src/js/components/list-posts/controllers/list-posts-excerpt-directive.controller.js
@@ -0,0 +1,5 @@
+angular.module('frontpress.components.list-posts').controller('ListPostsExcerptDirectiveController', ListPostsExcerptDirectiveController);
+
+function ListPostsExcerptDirectiveController(){
+	var vc = this;
+}
---
+++ src/js/components/list-posts/directives/list-posts-excerpt.directive.js
@@ -0,0 +1,28 @@
+angular.module('frontpress.components.list-posts').directive('listPostsExcerpt', ListPostsExcerptDirective);
+
+function ListPostsExcerptDirective($Frontpress){
+	var directive = {
+		scope: {
+			post: "=post"
+		},
+		restrict: 'E',
+		controller: 'ListPostsExcerptDirectiveController',
+		controllerAs: 'vc',
+		bindToController: true,
+		templateUrl: function(){
+			var templateUrl;
+			switch($Frontpress.apiVersion){
+				case "v2":
+				templateUrl = "/js/components/list-posts/templates/list-posts-excerpt-v2.template.html";
+				break;
+				case "v1":
+				templateUrl = "/js/components/list-posts/templates/list-posts-excerpt-v1.template.html";
+				break;				
+			}
+			return templateUrl;
+		}
+
+	};
+
+	return directive;
+}
