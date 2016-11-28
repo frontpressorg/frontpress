6d8ee842572e01dc186e32bfa524afc52686a457
Jota Teles <josetelesmaciel@gmail.com>
2016-11-28 00:56:40 -0200

    Closes #81 -  permite que :siteName seja usado nos posts

--- src/js/views/post/controllers/post-directive.controller.js
+++ src/js/views/post/controllers/post-directive.controller.js
@@ -1,6 +1,6 @@
 var module = angular.module("frontpress.views.post");
 
-function PostDirectiveController(FullPostModel, $stateParams, PageHeadModel, SlugsMapModel, $FrontPress, CategoriesApi, ApiManager){
+function PostDirectiveController(FullPostModel, $stateParams, PageHeadModel, SlugsMapModel, $FrontPress, CategoriesApi, ApiManager, BlogModel, $q){
 	var vc = this;
     vc.vm = FullPostModel;
     var postSlug = $stateParams.postSlug;
@@ -26,24 +26,33 @@ function PostDirectiveController(FullPostModel, $stateParams, PageHeadModel, Slu
     }
 
     var fullPostPromise = FullPostModel.loadFullPostById(postId);
+    var blogInformationPromise = BlogModel.getInformationPromise();
 
     PageHeadModel.init();
 
-    fullPostPromise.then(function(result){
 
-        var postTitle = ApiManager.getPath(result, "postTitle");
-        var postDateInfo = ApiManager.getPath(result, "postDate").getDateInfo();
+    var promises = [fullPostPromise, blogInformationPromise];
+    $q.all(promises).then(function(result){
+        
+        var fullPostResult = result[0];
+        var blogInformationResult = result[1];
+        
+        var postTitle = ApiManager.getPath(fullPostResult, "postTitle");
+        var postDateInfo = ApiManager.getPath(fullPostResult, "postDate").getDateInfo();
 
         var titleReplaceRules = {
             ":postTitle": postTitle,
             ":year": postDateInfo.year,
             ":month": postDateInfo.month,
             ":day": postDateInfo.day,
+            ":siteName": blogInformationResult.name,
+            ":siteDescription": blogInformationResult.description,
         };
 
         PageHeadModel.parsePageTitle("post", titleReplaceRules);
         vc.disqusId = FullPostModel.slug;
-	});
+
+    })
 }
 
 module.controller("PostDirectiveController", PostDirectiveController);
