0f02087d1c2dba384cb62cff1c97434cfbb87fc9
Jota Teles <josetelesmaciel@gmail.com>
2016-12-02 01:30:10 -0200

    [#92] Removendo algumas deps nao utilizadas

--- tasks/deploy.js
+++
@@ -1,9 +0,0 @@
-var gulp = require("gulp");
-var ghPages = require("gulp-gh-pages");
-
-module.exports = function() {
-    return gulp.task("deploy", function() {
-        return gulp.src("./build/**/*")
-            .pipe(ghPages());
-    })
-}
