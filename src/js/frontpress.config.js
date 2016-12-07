5c128baec76c0737d0945e30429d821aaff87eb3
Jota Teles <josetelesmaciel@gmail.com>
2016-12-07 07:56:31 -0200

    closes #87 - removido interpolate, html5mode e debuginfo

--- src/js/frontpress.config.js
+++ src/js/frontpress.config.js
@@ -1,12 +1,7 @@
 var module = angular.module("frontpress");
 
-function FrontPressConfig($interpolateProvider, $httpProvider, $urlRouterProvider, $locationProvider, BlogApiProvider, PostsApiProvider, $FrontPressProvider, $compileProvider){
-    $interpolateProvider.startSymbol("{[{").endSymbol("}]}");
-    $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded;charset=utf-8";
-    delete $httpProvider.defaults.headers.common["X-Requested-With"];
-    $locationProvider.html5Mode(true);
+function FrontPressConfig($FrontPressProvider){
     $FrontPressProvider.configure.load();    
-    $compileProvider.debugInfoEnabled(false);
 }
 
 module.config(FrontPressConfig);
