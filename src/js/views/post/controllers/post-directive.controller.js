c56dcff94f157083fe49142dd9c23657813737e4
Jota Teles <jose.maciel@vivadecora.com.br>
2016-12-22 23:45:32 -0200

    [#96] - tenta resolver Possibly unhandled rejection: {}

--- src/js/views/post/controllers/post-directive.controller.js
+++ src/js/views/post/controllers/post-directive.controller.js
@@ -49,7 +49,10 @@ function PostDirectiveController(FullPostModel, $stateParams, PageHeadModel, Slu
 
         PageHeadModel.parsePageTitle("post", titleReplaceRules);
         vc.disqusId = FullPostModel.slug;
+    })
 
+    $q.all(promises).catch(function(error){
+        console.error(error);
     })
 }
 
