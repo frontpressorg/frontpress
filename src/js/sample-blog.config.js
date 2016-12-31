1ac29384e926c51e186c41363edc781636f1aa96
Jota Teles <josetelesmaciel@gmail.com>
2016-12-30 22:16:11 -0200

    [#97] - refactoring gulp tasks to use release files in development

--- src/js/sample-blog.config.js
+++ src/js/sample-blog.config.js
@@ -1,28 +1,30 @@
-var module = angular.module("sample-blog");
+(function(){
+	
+	"use strict";
 
-function SampleBlogConfig($FrontPressProvider){
-    // $FrontPressProvider.configure.loadFromFile();
+	angular.module("sample-blog").config(SampleBlogConfig);
 
-	var configuration = {
-	"restApiUrl": "https://demo.wp-api.org/wp-json",
-	"apiVersion": "v2",
-	"pageSize": 5,
-	"infiniteScroll": true,
-	"routes": {
-		"post": "/:postYear/:postMonth/:postDay/:postSlug",
-		"home.pagination": "/pagina/:pageNumber"
-	},
-	"titles": {
-		"post": ":postTitle - :siteName",
-		"home": "Home - :siteName"
-	},
-	"overrides": {
-		"siteName": "FrontPress Default Template"
+	function SampleBlogConfig($FrontPressProvider){
+
+		var frontPressConfiguration = {
+			"restApiUrl": "https://www.vivadecora.com.br/revista/wp-json",
+			"apiVersion": "v2",
+			"pageSize": 5,
+			"infiniteScroll": true,
+			"routes": {
+				"post": "/:postYear/:postMonth/:postDay/:postSlug",
+				"home.pagination": "/pagina/:pageNumber"
+			},
+			"titles": {
+				"post": ":postTitle - :siteName",
+				"home": "Home - :siteName"
+			},
+			"overrides": {
+				"siteName": "FrontPress Default Template"
+			}
+		};
+
+		$FrontPressProvider.configure.load();  
 	}
-};
 
-	$FrontPressProvider.configure.load(configuration);  
-
-}
-
-module.config(SampleBlogConfig);
+})();
--- tasks/copy.js
+++ tasks/copy.js
@@ -1,46 +1,12 @@
 var gulp = require("gulp");
-var fs = require("fs");
 var merge = require("merge-stream");
 var concat = require("gulp-concat");
-var uglify = require("gulp-uglify");
-var iife = require("gulp-iife");
-var argv = process.argv;
 
 module.exports = function() {
-    var jsCopy = false;
     var angularCopy = false;
-    var otherDependenciesCopy = false;
     var javascriptDestFolder = "./build/js";
     var sampleBlogFiles = false;
-    var sampleBlogCopy  = false;
-    var parsedConfigFile = JSON.parse(fs.readFileSync("./frontpress.json", "utf8"));
-    var apiVersion = parsedConfigFile.apiVersion;
-    var infiniteScroll = parsedConfigFile.infiniteScroll;
-
-    var devFilesList = [
-        "./src/js/frontpress.template-cache.js",
-        "./src/js/frontpress.module.js",
-        "./src/js/frontpress.run.js",
-        "./src/js/frontpress.config.js",
-        "./src/js/apis/configs-to-params/**/*.module.js",
-        "./src/js/apis/configs-to-params/**/*.js",
-        "./src/js/apis/"+apiVersion+"/**/*.module.js",
-        "./src/js/apis/"+apiVersion+"/**/*.js",
-        "./src/js/{components,filters,views}/**/*.module.js",
-        "./src/js/{components,filters,views}/**/*.run.js",
-        "./src/js/{components,filters,views}/**/*.factory.js",
-        "./src/js/{components,filters,views}/**/*.directive.js",
-        "./src/js/{components,filters,views}/**/*.value.js",
-        "./src/js/{components,filters,views}/**/*.constant.js",
-        "!**/*.tests.js",
-        "./src/js/{components,filters,views}/**/*.js"
-    ];
-
-    var dependenciesFiles = [
-        "./node_modules/angular-ui-router/release/angular-ui-router.js",
-        "./node_modules/ng-infinite-scroll/build/ng-infinite-scroll.js",
-        "./node_modules/angular-disqus/src/angular-disqus.js",
-    ];
+    var sampleBlogCopy  = false;    
 
     var sampleBlogFiles = [
         "./src/js/sample-blog.module.js",
@@ -48,41 +14,18 @@ module.exports = function() {
         "./src/js/sample-blog-main.controller.js"
     ];
 
-    angularCopy = gulp.src(["./node_modules/angular/angular.min.js"])
+    angularCopy = gulp.src(["./node_modules/angular/angular.min.js", "./node_modules/angular/angular.js"])
         .pipe(gulp.dest(javascriptDestFolder));
-
-    otherDependenciesCopy = gulp.src(dependenciesFiles)
-        .pipe(concat("dev/external.js"))
-        .pipe(gulp.dest(javascriptDestFolder));
-
-    jsCopy = gulp.src(devFilesList)
-        .pipe(iife({ prependSemicolon: false }))
-        .pipe(concat("dev/main.js"))
-        .pipe(gulp.dest(javascriptDestFolder));
-
-    if (argv.indexOf("--production") !== -1) {
-        jsCopy = gulp.src([
-            javascriptDestFolder + "/dev/external.js",
-            javascriptDestFolder + "/dev/main.js"
-        ])
-        .pipe(concat("main.js"))
-        .pipe(uglify({mangle: false}))
-        .pipe(gulp.dest(javascriptDestFolder));
-    }
 
     sampleBlogCopy = gulp.src(sampleBlogFiles)
         .pipe(concat("dev/sample-blog.js"))
         .pipe(gulp.dest(javascriptDestFolder));
 
-    var othersCopy = gulp.src([
-        "./src/js/**/*.html",
-    ])
-    .pipe(gulp.dest("./build/js"));
 
     var releaseCopy = gulp.src([
         "./release/**/*.js"
     ])
-    .pipe(gulp.dest("./build/release"));
+    .pipe(gulp.dest("./build/js/release"));    
 
-    return merge(angularCopy, otherDependenciesCopy, othersCopy, jsCopy, sampleBlogCopy, releaseCopy);
+    return merge(angularCopy, releaseCopy, sampleBlogCopy);
 };
