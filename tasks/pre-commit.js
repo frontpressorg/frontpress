bcf2d4e66a9d49f3da9bd3f29b70a60ddc01444d
Dinesh Trivedi <dineshat@amazon.com>
2016-12-31 11:16:43 +0200

    [#104] - Corrigindo pontos levantados pelo teles

--- tasks/pre-commit.js
+++ tasks/pre-commit.js
@@ -1,10 +1,15 @@
 "use strict";
 
 var gulp = require("gulp"),
-  symlink = require("gulp-sym");
+   p = require('path'),
+   symlink = require("gulp-sym");
 
 module.exports = function() {
     return gulp.task("pre-commit", function() {
-        gulp.src('ci/pre-commit').pipe(symlink('.git/hooks/pre-commit'));
+         gulp.src('ci/pre-commit').pipe(symlink(function(source) {
+
+            return p.join('.git/hooks/', source.relative.split(p.sep)[0]);
+
+         }, { force: true }));
     });
 };
