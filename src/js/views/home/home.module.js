8d8af8d2cb56e028baf5f38fb69db35d3a985e6d
Jota Teles <josetelesmaciel@gmail.com>
2017-01-02 19:22:58 -0200

    [#109] testing travis deploy

--- src/js/views/home/home.module.js
+++ src/js/views/home/home.module.js
@@ -3,9 +3,9 @@
 angular.module("frontpress.views.home",
 				["ui.router",
 				"infinite-scroll",
-				"frontpress.components.list-posts",
-				"frontpress.components.pagination",
-				"frontpress.components.page-head",
 				"frontpress.components.api-manager",
+				"frontpress.components.pagination",
+				"frontpress.components.list-posts",
+				"frontpress.components.page-head",
 				"frontpress.components.blog",
 				"frontpress.components.frontpress-provider"]);
